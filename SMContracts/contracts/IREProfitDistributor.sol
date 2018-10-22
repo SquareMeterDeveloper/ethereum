214b1b60f89a3137ac29a443f227d4c051cfb721
Junbin Hao <junbin.hao@hotmail.com>
2018-10-22 17:03:39 +0800

    租金分配余数方式调整

--- SMContracts/contracts/IREProfitDistributor.sol
+++ SMContracts/contracts/IREProfitDistributor.sol
@@ -22,36 +22,27 @@ contract IREProfitDistributor is ERC721ProfitDistributor {
         return tokenId;
     }
 
-    function calculate(ERC20 cnyToken, ERC20 ireToken, address payer) private view returns (uint, uint, uint){
-        uint balance = cnyToken.allowance(payer, this);
-        //总份数=总平米数*分割倍数
-        uint totalShares = ireToken.totalSupply();
-        //计算每份需分配租金数，余数部分不分配
-        uint amount = balance / totalShares;
-        uint total = amount * totalShares;
-        return (total, balance - total, amount);
-    }
-
     //分配租金根据持股数和合约资金账户余额进行租金分配
     //注：租金属于该合约的账户，充值需冲入该合约地址，分配租金是逐笔转账的过程
     function pay(uint taskId) onlyOperator() external {
         ERC20 cnyToken = getCny();
         ERC20 ireToken = getAsset();
+        uint balance = cnyToken.allowance(msg.sender, this);
+        uint totalShares = ireToken.totalSupply();
         uint total;
-        uint residual;
-        uint amt;
-        (total, residual, amt) = calculate(cnyToken, ireToken, msg.sender);
-        if (amt > 0) {
+        if (balance > 0) {
             uint holders = ireToken.holdersCount();
             for (uint i = 0; i < holders; i++) {
                 address adr;
                 uint shares;
                 (adr, shares) = ireToken.holderAt(i);
+                uint amount = balance * shares / totalShares;
+                total += amount;
                 if (shares > 0) {
-                    cnyToken.transferFrom(msg.sender, adr, shares * amt, taskId);
+                    cnyToken.transferFrom(msg.sender, adr, amount, taskId);
                 }
             }
-            Pay(msg.sender, taskId, total, residual, 0);
+            Pay(msg.sender, taskId, total, balance - total, 0);
         } else {
             Pay(msg.sender, taskId, 0, 0, 1);
         }
